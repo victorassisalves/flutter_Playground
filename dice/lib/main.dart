64f604f4d19d0118d1df6d39881a1e3b0b684551
Victor Assis <victor.assis@onsurance.me>
2020-01-20 10:51:06 -0300

    Tirei a bandeira de debug

--- dice/lib/main.dart
+++ dice/lib/main.dart
@@ -5,6 +5,7 @@ import 'package:flutter/material.dart';
 void main() {
   return runApp(
     MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         backgroundColor: Colors.red,
         appBar: AppBar(
