b5941fb4c77d9b4730924bb6ff94ae41b259f11b
Victor Assis <victor.assis@onsurance.me>
2020-01-20 10:49:26 -0300

    Projeto de lancamento de dados. Geração de números alatórios

---
+++ dice/lib/main.dart
@@ -0,0 +1,69 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+
+void main() {
+  return runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.red,
+        appBar: AppBar(
+          title: Text('Dice App'),
+          centerTitle: true,
+          backgroundColor: Colors.red,
+        ),
+        body: DiceAppBody(),
+      ),
+    ),
+  );
+}
+
+class DiceAppBody extends StatefulWidget {
+  @override
+  _DiceAppBodyState createState() => _DiceAppBodyState();
+}
+
+class _DiceAppBodyState extends State<DiceAppBody> {
+  int topDiceNumber = 1;
+  int bottomDiceNumber = 1;
+  void generateRandomNumber() {
+    setState(() {
+      bottomDiceNumber = Random().nextInt(6) + 1;
+      topDiceNumber = Random().nextInt(6) + 1;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        children: <Widget>[
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: FlatButton(
+                highlightColor: Colors.redAccent,
+                onPressed: () {
+                  generateRandomNumber();
+                },
+                child: Image.asset('images/dice$topDiceNumber.png'),
+              ),
+            ),
+          ),
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: FlatButton(
+                highlightColor: Colors.redAccent,
+                onPressed: () {
+                  generateRandomNumber();
+                },
+                child: Image.asset('images/dice$bottomDiceNumber.png'),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
